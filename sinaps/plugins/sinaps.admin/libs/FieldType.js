506d2f5b7b5526e571a6dab912f2455df9687a83
Michael Grenier <michael.grenier@live.com>
2016-01-11 22:41:16 -0500

    inline form

--- sinaps/plugins/sinaps.admin/libs/FieldType.js
+++ sinaps/plugins/sinaps.admin/libs/FieldType.js
@@ -31,14 +31,19 @@ function FieldType (options) {
 		},
 
 		getFieldTemplate: function (field) {
-			return '<div class="form-group">\
-	<label for="{{ field.id|default(field.name) }}">{{ field.label }} {% if field.locale %}<small><code>{{ field.locale|upper }}</code></small>{% endif %}</label>\
-	'+ this.getInputTemplate(field) +'\
-	{% if field.instructions %}<small class="text-muted">{{ field.instructions }}</small>{% endif %}\
-	{% if field.success %}<small class="text-success">{{ field.success }}</small>{% endif %}\
-	{% if field.info %}<small class="text-info">{{ field.info }}</small>{% endif %}\
-	{% if field.warning %}<small class="text-warning">{{ field.warning }}</small>{% endif %}\
-	{% if field.error %}<small class="text-error">{{ field.error }}</small>{% endif %}\
+			return '<div class="form-group row">\
+	<label class="col-xs-12 {% if field.inline %}col-md-2 form-control-label-md{% endif %}" for="{{ field.id|default(field.name) }}">\
+		{{ field.label }}\
+		{% if field.locale %}<small><code>{{ field.locale|upper }}</code></small>{% endif %}\
+	</label>\
+	<div class="col-xs-12 {% if field.inline %}col-md-10{% endif %}">\
+		'+ this.getInputTemplate(field) +'\
+		{% if field.instructions %}<small class="text-muted">{{ field.instructions }}</small>{% endif %}\
+		{% if field.success %}<small class="text-success">{{ field.success }}</small>{% endif %}\
+		{% if field.info %}<small class="text-info">{{ field.info }}</small>{% endif %}\
+		{% if field.warning %}<small class="text-warning">{{ field.warning }}</small>{% endif %}\
+		{% if field.error %}<small class="text-error">{{ field.error }}</small>{% endif %}\
+	</div>\
 </div>';
 		},
 
